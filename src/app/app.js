3755079bfb4075139b0b8c7dc9a84efe3c37cd01
Matt Burke <Burke9077@gmail.com>
2023-06-18 02:23:28 -0400

    Shared DB connection

--- src/app/app.js
+++ src/app/app.js
@@ -1,49 +1,80 @@
 require('dotenv').config();
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
 
-// Create a new express application instance
-const app = express();
-
-// The port the express app will listen on
-const port = process.env.SERVER_PORT || 3000;
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-// Configure MySQL connection
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
-
-// Connect to MySQL
-connection.connect((err) => {
-  // Log any errors to the console
-  if (err) {
-    console.error('Error connecting to the database', err);
-    return;
+// Check if the current process is the master process
+if (cluster.isMaster) {
+  // Fork worker processes based on the environment variable config or the number of CPUs, whichever is less
+  let numWorkers = process.env.WORKER_THREADS > numCPUs ? numCPUs : process.env.WORKER_THREADS;
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
   }
-  console.log('Connected to the MySQL server');
 
-  // If the error is fatal, exit the process
-  if (err && typeof err === 'object' && err.hasOwnProperty('fatal') && err.fatal === true) {
-    console.error('Fatal error occurred, exiting process');
-    process.exit(1);
-  }
-});
+  // Log when a worker process is online
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
 
-// Sample endpoint
-app.get('/', (req, res) => {
-  res.send('Hello, Welcome to our social media site!');
-});
+  // Log if a worker process exits
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} exited with code ${code} and signal ${signal}`);
+    // Restart the worker process
+    cluster.fork();
+  });
+} else {
+  // Create a new express application instance
+  const app = express();
 
-// Serve the application at the given port
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  // The port the express app will listen on
+  const port = process.env.SERVER_PORT || 3000;
+
+  // Middleware
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  // Create a shared MySQL connection
+  const sharedConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+
+  // Connect to MySQL
+  sharedConnection.connect((err) => {
+    // Log any errors to the console
+    if (err) {
+      console.error('Error connecting to the database', err);
+      return;
+    }
+    console.log('Connected to the MySQL server');
+
+    // If the error is fatal, exit the process
+    if (err && typeof err === 'object' && err.hasOwnProperty('fatal') && err.fatal === true) {
+      console.error('Fatal error occurred, exiting process');
+      process.exit(1);
+    }
+  });
+
+  // Sample endpoint
+  app.get('/', (req, res) => {
+    // Use the shared MySQL connection for database operations
+    sharedConnection.query('SELECT * FROM your_table', (err, results) => {
+      if (err) {
+        console.error('Error executing MySQL query', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.send('Hello, Welcome to our social media site!');
+    });
+  });
+
+  // Serve the application at the given port
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} is running and listening on port ${port}`);
+  });
+}
