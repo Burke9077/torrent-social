3f7372de2722d7b2fd1cfe6ee0c3129cf4c90ddc
Matt Burke <Burke9077@gmail.com>
2023-06-17 22:59:20 -0400

    Fixes image replacement

--- localdev.sh
+++ localdev.sh
@@ -9,11 +9,16 @@ timestamp=$(date +%s%3N)
 nerdctl --namespace=k8s.io build -t local/torrent-social-app:$timestamp ./src/app
 
 # Set up variables
-TORRENT_SOCIAL_APP_IMAGE="local/torrent-social-app:$timestamp"
+export TORRENT_SOCIAL_APP_IMAGE="local/torrent-social-app:$timestamp"
+
+# Find or create the mysql configuration
+export DB_HOST="${DB_HOST:-primarydb}"
+export DB_USER="${DB_USER:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12)}" # Take the username or generate a random one
+export DB_PASS="${DB_PASS:-$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12)}" # Take the password or generate a random one
+export DB_NAME="${ENVIRONMENT_VARIABLE_NAME:-torrent_social}"
 
 # Pull the template kubernetes file and make changes as needed
-k8sfile=`cat "k8s-manifest-template.yaml"`
-k8sfile=`echo "$k8sfile" | sed "s#{{TORRENT_SOCIAL_APP_IMAGE}}#$TORRENT_SOCIAL_APP_IMAGE#g"`
+k8sfile=`envsubst < k8s-manifest-template.yaml`
 
 # Finally apply the changes to kubernetes
 echo "$k8sfile" | kubectl apply -f -
