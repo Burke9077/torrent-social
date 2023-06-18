d12095ebd1d03b179c5e19c119dc48fbb5b20b0f
Matt Burke <Burke9077@gmail.com>
2023-06-17 22:34:00 -0400

    Automatically replaces image using timestamp based tag

--- localdev.sh
+++ localdev.sh
@@ -2,8 +2,18 @@
 
 # Run this file to start up the local development environment
 
-# Build our docker images as needed
-nerdctl --namespace=k8s.io build -t local/torrent-social-app:latest ./src/app
+# First get the current timestamp in millis to use in the docker image tag
+timestamp=$(date +%s%3N)
+
+# Build our docker images as needed with the timestamp in the tag
+nerdctl --namespace=k8s.io build -t local/torrent-social-app:$timestamp ./src/app
+
+# Set up variables
+TORRENT_SOCIAL_APP_IMAGE="local/torrent-social-app:$timestamp"
+
+# Pull the template kubernetes file and make changes as needed
+k8sfile=`cat "k8s-manifest-template.yaml"`
+k8sfile=`echo "$k8sfile" | sed "s#{{TORRENT_SOCIAL_APP_IMAGE}}#$TORRENT_SOCIAL_APP_IMAGE#g"`
 
 # Finally apply the changes to kubernetes
-kubectl apply -f ./k8s-localdev.yaml
+echo "$k8sfile" | kubectl apply -f -
